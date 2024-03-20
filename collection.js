11a832e909f7546e4729e4aeaa0212caacb11022
MohamedSulaiman8 <112604544+MohamedSulaiman8@users.noreply.github.com>
2024-03-20 10:28:19 +0530

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+var productContainer=document.querySelector(".products")
+var search=document.querySelector("#search")
+var products=productContainer.querySelectorAll("div")
+search.addEventListener("keyup",function (event){
+    var eventValue=event.target.value.toUpperCase()
+    for(i=0;i<products.length;i++){
+        var productname=products[i].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(eventValue)<0){
+            products[i].style.display="none"
+        }
+        else{
+            products[i].style.display="block"
+        }
+    }
+=======
+var productContainer=document.querySelector(".products")
+var search=document.querySelector("#search")
+var products=productContainer.querySelectorAll("div")
+search.addEventListener("keyup",function (event){
+    var eventValue=event.target.value.toUpperCase()
+    for(i=0;i<products.length;i++){
+        var productname=products[i].querySelector("p").textContent
+        if(productname.toUpperCase().indexOf(eventValue)<0){
+            products[i].style.display="none"
+        }
+        else{
+            products[i].style.display="block"
+        }
+    }
+>>>>>>> d22734503986e420fb52c500d340bfa3ddfad50a
+})
---
+++ script.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+var sideBar=document.querySelector(".side-navbar")
+
+function showSideNavbar(){
+    sideBar.style.left="0";
+}
+function closeSideNavbar(){
+    sideBar.style.left="-60%";
+=======
+var sideBar=document.querySelector(".side-navbar")
+
+function showSideNavbar(){
+    sideBar.style.left="0";
+}
+function closeSideNavbar(){
+    sideBar.style.left="-60%";
+>>>>>>> d22734503986e420fb52c500d340bfa3ddfad50a
+}
